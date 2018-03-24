e0257f4a2c2d3f739a4cd0b1d1b0275d669f897f
andrew <andrew@andrew-Lenovo-ideapad-110-15ACL>
2018-03-24 18:45:59 -0400

    Projects 0.1

--- src/Divider.java
+++ src/Divider.java
@@ -7,6 +7,15 @@ public class Divider {
         Scanner reader = new Scanner(System.in);
             
         // Implement your program here. Remember to ask the input from user.
-        asdfasfsdfasdf
+        System.out.print("Type a number:  ");
+        int a = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type a number:  ");
+        int b = Integer.parseInt(reader.nextLine());
+        
+        double result = 1.0 * a / b;
+        
+        System.out.print("Division:  " + a + " / " + b + " = " + result);
+                
     }
 }
