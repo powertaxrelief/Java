1ff217914de3b50e203145aa7eb35fbad780b784
powertaxrelief <30771692+powertaxrelief@users.noreply.github.com>
2018-03-24 20:43:11 -0400

    0.12
    Changed variables to be less vague, more descriptive

--- week1-011.BiggerNumber/src/BiggerNumber.java
+++ week1-011.BiggerNumber/src/BiggerNumber.java
@@ -8,12 +8,12 @@ public class BiggerNumber {
 
         // Implement your program here. Remember to ask the input from user
         System.out.print("Type a number:  ");
-        int a = Integer.parseInt(reader.nextLine());
+        int num1 = Integer.parseInt(reader.nextLine());
         
         System.out.print("Type another number:  ");
-        int b = Integer.parseInt(reader.nextLine());
+        int num2 = Integer.parseInt(reader.nextLine());
         
-        int result = Math.max(a,b);
+        int result = Math.max(num1, num2);
         
         System.out.print("The bigger number of the two numbers given was:  " + result);
     }
