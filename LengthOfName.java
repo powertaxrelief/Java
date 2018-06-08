fe5629e7653a5747784890e3e507dede74d103ff
powertaxrelief <30771692+powertaxrelief@users.noreply.github.com>
2018-06-08 11:01:29 -0400

    Update LengthOfName.java

--- LengthOfName.java
+++ LengthOfName.java
@@ -5,9 +5,22 @@ public class LengthOfName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // call your method from here
+        
+        System.out.print("Type your name:  ");
+        
+        String name = string.parseString(nextLine());
+        
+        System.out.println("Number of characters:  " + name(name));
+        
     }
     
+    
+    public static int calculateCharacters(String name) {
+        
     // do here the method
     // public static int calculateCharacters(String text)
+    int nameLength = name.length();
     
+    return nameLength;
+        
 }
